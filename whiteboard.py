4ff845f859520df3a665adba97d06f9820ac6c3a
CryptoDoopo123 <101756236+CryptoDoopo123@users.noreply.github.com>
2022-04-26 12:48:29 -0600

    Add files via upload
    Changed chance attribute to dice and changed possibility function to dice roll.
    changed the does hit function to get the desired lightning function.

--- whiteboard.py
+++ whiteboard.py
@@ -2,50 +2,51 @@ import random
 
 
 class Creature:
-    def __init__(self, name="default", health=10, damage=1, armor=10, chance=20):
+    def __init__(self, name="default", health=10, damage=1, armor=10, dice=20):
         self.name = name
         self.health = health
         self.damage = damage
         self.armor = armor
-        self.chance = chance
+        self.dice = dice
 
     def heal(self, heal_points):
         if heal_potion == True:
             self.health += heal_points
 
     def attack(self, target):
-        target.health -= (self.damage * does_hit(self, target))
+        target.health -= (self.damage * does_hit(self,
+                          target, dice_roll(self)))
 
     def take_damage(self):
         self.health -= self.damage
 
 
 class Wizard(Creature):
-    def wiz_lightning(self, target_1, target_2):
-        average = (target_1.armor + target_2.armor) // 2
-        temp = Creature("temp", 1, 1, average, 1)
-        chance = does_hit(self, temp)
-        target_1.health -= (self.damage * chance)
-        target_2.health -= (self.damage * chance)
+    def lightning(self, *targets):
+        chance = dice_roll(self)
+        for target in targets:
+            target.health -= (self.damage * does_hit(self, target, chance))
 
 
 class Warrior(Creature):
     def ax(self, target):
-        target.health -= (2 * self.damage * does_hit(self, target))
+        target.health -= (2 * self.damage *
+                          does_hit(self, target, dice_roll(self)))
 
 
 class Ranger(Creature):
     def boaw_arrow(self, target):
-        target.health -= (self.damage * does_hit(self, target))
-        target.health -= (self.damage * does_hit(self, target))
+        target.health -= (self.damage * does_hit(self,
+                          target, dice_roll(self)))
+        target.health -= (self.damage * does_hit(self,
+                          target, dice_roll(self)))
 
 
-def possibility(self):
-    return random.randint(1, self.chance)
+def dice_roll(self):
+    return random.randint(1, self.dice)
 
 
-def does_hit(self, target):
-    rn = possibility(self)
+def does_hit(self, target, rn):
     if rn == 1:
         self.take_damage()
         return(rn >= target.armor)
