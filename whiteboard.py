1b7cb2bd0498543d5edda4e8054a9daafc6e053d
CryptoDoopo123 <101756236+CryptoDoopo123@users.noreply.github.com>
2022-04-26 12:00:30 -0600

    Add files via upload
    Cleaned up the test elements to a temporary file.
    added armor and changed the possibility function

--- whiteboard.py
+++ whiteboard.py
@@ -2,10 +2,11 @@ import random
 
 
 class Creature:
-    def __init__(self, name="default", health=10, damage=1, chance=50):
+    def __init__(self, name="default", health=10, damage=1, armor=10, chance=20):
         self.name = name
         self.health = health
         self.damage = damage
+        self.armor = armor
         self.chance = chance
 
     def heal(self, heal_points):
@@ -13,62 +14,41 @@ class Creature:
             self.health += heal_points
 
     def attack(self, target):
-        target.health -= (self.damage * possibility(self))
+        target.health -= (self.damage * possibility(self, target))
 
 
 class Wizard(Creature):
     def wiz_lightning(self, target_1, target_2):
-        chance = possibility(self)
+        average = (target_1.armor + target_2.armor) // 2
+        temp = Creature("temp", 1, 1, average, 1)
+        chance = possibility(self, temp)
         target_1.health -= (self.damage * chance)
         target_2.health -= (self.damage * chance)
 
 
 class Warrior(Creature):
-    def ax(self):
-        pass
+    def ax(self, target):
+        target.health -= (2 * self.damage * possibility(self, target))
 
 
 class Ranger(Creature):
-    def boaw_arrow(self):
-        pass
+    def boaw_arrow(self, target):
+        target.health -= (self.damage * possibility(self, target))
+        target.health -= (self.damage * possibility(self, target))
 
 
-def possibility(self):
-    rn = random.randint(1, 100)
-    return(rn >= self.chance)
+def possibility(self, target):
+    rn = random.randint(1, self.chance)
+    if rn == 1:
+        self.take_damage()
+        return(rn >= target.armor)
+    elif rn == self.chance:
+        return((rn >= target.armor) * 2)
+    else:
+        return(rn >= target.armor)
 
 
 heal_potion = False
-x = Creature("test", 11, 2, 50)
-y = Creature("bot", 5, 3, 75)
-w = Wizard("wiz", 8, 4, 25)
-
-hero_name = input("please type your characters name : ")
-hero_class = input("please type class; wizard, warrior, or ranger : ")
-if hero_class.lower() == "warrior":
-    hero = Warrior(hero_name, 10, 5)
-elif hero_class.lower() == "wizard":
-    hero = Wizard(hero_name, 8, 3, 25)
-elif hero_class.lower() == "ranger":
-    hero = Ranger(hero_name, 10, 4, 33)
-else:
-    pass
-
-
-# test the features code
-
-# print("bot health", y.health)
-# print("test health", x.health)
-# w.wiz_lightning(x, y)
-# print("bot health after attack", y.health)
-# print("test health after attack", x.health)
-# print("bot health", y.health)
-# x.attack(y)
-# print("bot health after attack", y.health)
-# print("test health", x.health)
-# x.heal(heal_potion)
-# print("test health after heal", x.health)
-# print(hero.name)
-# print(hero.health)
-# print(hero.damage)
-# print(hero.chance)
+x = Creature("test", 11, 2, 10, 50)
+y = Creature("bot", 5, 3, 7, 75)
+w = Wizard("wiz", 8, 4, 10, 25)
